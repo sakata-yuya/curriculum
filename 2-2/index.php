1f659640212df022bff848ec0944b1d8f8e91794
sakata-yuya <yuya7954@gmail.com>
2020-06-24 15:30:00 +0900

    2-2
    変数とデータ型

---
+++ 2-2/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$message = "Hello World";
+var_dump ($message);
+
+echo "<br>";
+
+$x = 8;
+$y = 5.55;
+var_dump ($x);
+
+echo "<br>";
+
+var_dump ($y);
+echo "<br>";
+
+$flag = true;
+var_dump($flag);
+echo "<br>";
+
+$emp = null;
+var_dump($emp);
+echo "<br>";
+
+$en_words = 'Hello World';
+var_dump($en_words);
+echo '<br>';
+$ja_words = 'こんにちは';
+var_dump($ja_words);
+echo "<br>";
+
+echo "1.API"."<br>";
+echo "アプリケーションをプログラミングするためのインターフェース"."<br>";
+echo "2.SVN"."<br>";
+echo "バージョン管理システムの一つ"."<br>";
+?>
