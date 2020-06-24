ca13980c18c8b0ef2a68f0d8788d6fabcab9f9af
sakata-yuya <yuya7954@gmail.com>
2020-06-24 21:49:52 +0900

    2-4
    演算子

---
+++ 2-4/index.php
@@ -0,0 +1,66 @@
+<?php
+
+$x = 5;
+
+echo "Hello"."World";
+
+echo "<br>";
+
+$hello = "Hello";
+$world = "World";
+echo $hello.$world;
+
+echo "<br>";
+
+echo 3 + 5;
+echo '<br>';
+echo 10 - 7;
+echo '<br>';
+echo 5 * 8;
+echo '<br>';
+echo 12 / 4;
+echo '<br>';
+echo 7 % 3;
+
+echo '<br>';
+
+// 単項演算子
+// $x = $x + 1
+$x++; 
+echo $x;
+echo '<br>';
+// $x = $x - 1
+$x--;
+echo $x;
+
+echo '<br>';
+
+$x = 6;
+
+$x += 5;
+echo $x;
+
+echo '<br>';
+
+$x *= 5;
+echo $x;
+
+echo '<br>';
+
+$x -= 5;
+echo $x;
+
+echo '<br>';
+
+$x /= 5;
+echo $x;
+
+echo '<br>';
+
+echo "1.エピデンス"."<br>";
+echo "システムが発注どおりに動作していることを示す証拠となる書類やデータなどのこと"."<br>";
+echo "2.バッチ"."<br>";
+echo "あらかじめ処理に必要な指示と要素を与えておき、一気に処理をすること"."<br>";
+echo "3.MVCモデル"."<br>";
+echo "プログラムの処理を役割毎に分けて開発を行うこと"."<br>";
+?>
