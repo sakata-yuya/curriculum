f4bd5bbc6a50c63d54ca31c8b12435d35b0b2566
sakata-yuya <yuya7954@gmail.com>
2020-06-24 21:24:14 +0900

    2-3
    定数

---
+++ 2-3/index.php
@@ -0,0 +1,20 @@
+<?php
+// index.php
+// 変数：データにつけるラベル（変更可能）
+// 定数：変更不可
+
+define("ADMIN_EMAIL", "bryu17090@g.nihon-u.ac.jp");
+define("LIST_COUNT", 15);
+echo ADMIN_EMAIL;
+echo '<br>';
+echo LIST_COUNT;
+
+echo '<br>';
+
+echo "1.デプロイ"."<br>";
+echo "ソフトウェアを実際の運用環境に配置・展開して利用できるようにすること"."<br>";
+echo "2.バリデーション"."<br>";
+echo "バージョン管理システムの一つ"."<br>";
+echo "3.アジャイル・ウォーターフォール"."<br>";
+echo "バージョン管理システムの一つ"."<br>";
+?>
