a6dc3314c050f9fd83953ec1fd82c634e3ed03c0
sakata-yuya <yuya7954@gmail.com>
2020-06-25 23:55:53 +0900

    2-11
    フォーム

---
+++ 2-11/index.php
@@ -0,0 +1,20 @@
+
+<form action="result.php" method="post">
+名前：<input type="text" name="my_name" />
+<br>
+メールアドレス:<input type="email" name="email">
+<br>
+パスワード：<input type="password" name="password" />
+<br>
+<input type="submit" value="送信" />
+</form>
+
+<?php
+echo "<br>";
+echo "1.仕様書・設計書"."<br>";
+echo "何を作るのか・どうやって作るか"."<br>";
+echo "2.Git"."<br>";
+echo "プログラムソースなどの変更履歴を管理する分散型のバージョン管理システムのこと"."<br>";
+echo "3.マージツール"."<br>";
+echo "複数のファイルやデータ、プログラムなどを、決められたルールに従って一つに統合するツール"."<br>";
+?>
