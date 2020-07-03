5cef7cdd612628ff08f0c7e4626710a395663645
sakata-yuya <yuya7954@gmail.com>
2020-07-03 15:15:46 +0900

    3-3

---
+++ 3-3/bugTest/test2/dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+// セッション開始
+session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "dbuser";
+// ユーザー名のパスワード
+$db['pass'] = "hogehoge";
+// データベース名
+$db['dbname'] = "YIGroupBlog";
+
