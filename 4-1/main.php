80aa4cd6afad586097561577f5a11f2e23b8b789
sakata-yuya <yuya7954@gmail.com>
2020-07-16 01:43:44 +0900

    4-1

---
+++ 4-1/main.php
@@ -0,0 +1,62 @@
+<?php
+// セッション開始
+session_start();
+// セッションにuser_nameの値がなければlogin.phpにリダイレクト
+if (empty($_SESSION["user_name"])) {
+    header("Location: login.php");
+    exit;
+}
+
+require_once('db_connect.php');
+require_once('function.php');
+
+
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>メイン</title>
+</head>
+<body>
+    <h1>メインページ</h1>
+    <p>ようこそ<?php echo $_SESSION["user_name"]; ?>さん</p>
+    <a href="logout.php">ログアウト</a>
+
+<?php
+$sql = "SELECT * FROM posts order by id desc";
+$pdo = db_connect();
+try {
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+?>
+<table>
+    <tr>
+        <td>記事ID</td>
+        <td>タイトル</td>
+        <td>本文</td>
+        <td>投稿日</td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+        <tr>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['title']; ?></td>
+            <td><?php echo $row['content']; ?></td>
+            <td><?php echo $row['time']; ?></td>
+            <td><a href="detail_post.php?id=<?php echo $row['id']; ?>">詳細</a></td>
+            <td><a href="edit_post.php?id=<?php echo $row['id']; ?>">編集</a></td>
+            <td><a href="delete_post.php?id=<?php echo $row['id']; ?>">削除</a></td>
+        </tr>
+    <?php } ?>
+</table>
+<?php   
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+    die();
+}
+?>
+</body>
+</html>
