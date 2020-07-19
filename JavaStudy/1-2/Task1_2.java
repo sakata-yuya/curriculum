61f884a76a8cca250d48fe099d560be524d73177
sakata-yuya <yuya7954@gmail.com>
2020-07-20 02:28:14 +0900

    JavaStudy1-2

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -31,7 +31,7 @@ public class Task1_2 {
         int[] intArray = { 10, 20, 30, 40, 50 };
 
         // ⑥下記の処理について、何をしているのかコメントを記入してください。
-        // intArray配列の2番目と5番目の要素を出力している
+        // intArray配列の2番目と5番目の要素を出力して、足している
         System.out.println(intArray[1] + intArray[4]);
 
     }
